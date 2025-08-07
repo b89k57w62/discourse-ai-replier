be5c091402091c3f3063656e6081e93a12c28aec
Jeffrey Wu <wu19930628@gmail.com>
2025-08-07 12:26:46 +0800

    fix: remove database access during bootstrap phase
    Remove SiteSetting check from after_initialize to prevent database
    access during bootstrap. The scheduled job will be automatically
    registered by Discourse through the 'every 1.hour' declaration.

--- plugin.rb
+++ plugin.rb
@@ -14,6 +14,7 @@ gem 'faraday-net_http', '3.0.2', require: false
 register_asset 'stylesheets/discourse-ai-replier.scss'
 
 after_initialize do
+  # Load core modules
   %w[
     lib/ai_replier/rate_limiter
     lib/ai_replier/health_checker
@@ -24,6 +25,7 @@ after_initialize do
     require_relative file
   end
 
+  # Load jobs
   %w[
     app/jobs/scheduled/ai_topic_selector_job
     app/jobs/regular/ai_create_reply_job
@@ -31,7 +33,7 @@ after_initialize do
     require_relative file
   end
 
-  if SiteSetting.ai_replier_enabled?
-    Jobs.enqueue_in(1.minute, :ai_topic_selector) if Rails.env.production?
-  end
+  # The scheduled job will be automatically registered by Discourse
+  # due to the `every 1.hour` declaration in AiTopicSelector class
+  # No need to manually enqueue or check settings here
 end
