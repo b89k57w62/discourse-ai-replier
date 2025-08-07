7c922a6abc00aab2face85eec3f1189875e28aa2
Jeffrey Wu <wu19930628@gmail.com>
2025-08-07 12:27:39 +0800

    style: clean up plugin.rb formatting and remove comments
    Remove unnecessary comments and ensure proper file ending

--- plugin.rb
+++ plugin.rb
@@ -14,7 +14,6 @@ gem 'faraday-net_http', '3.0.2', require: false
 register_asset 'stylesheets/discourse-ai-replier.scss'
 
 after_initialize do
-  # Load core modules
   %w[
     lib/ai_replier/rate_limiter
     lib/ai_replier/health_checker
@@ -25,15 +24,10 @@ after_initialize do
     require_relative file
   end
 
-  # Load jobs
   %w[
     app/jobs/scheduled/ai_topic_selector_job
     app/jobs/regular/ai_create_reply_job
   ].each do |file|
     require_relative file
   end
-
-  # The scheduled job will be automatically registered by Discourse
-  # due to the `every 1.hour` declaration in AiTopicSelector class
-  # No need to manually enqueue or check settings here
 end
